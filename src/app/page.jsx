fdb979735eed8f10c34184368a6ecff5470cb9d2
JAPlaisted <jonathan.plaisted@outlook.com>
2025-08-22 19:31:07 -0400

    globals

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <main className="p-8">
-      <h1 className="heading--xl">Kitchen Sink</h1>
-      <p className="mt-4 body--l">navigate to <a href="/kitchen-sink">/kitchen-sink</a></p>
+      <h1 className="heading--xl text-center">Kitchen Sink</h1>
+      <p className="mt-4 body--l text-center">navigate to <a href="/kitchen-sink">/kitchen-sink</a></p>
     </main>
   );
 }
